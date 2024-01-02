6e5474831cee372d773f8fd2f77d725442cc601b
Ashish Kumar <135847311+i-am-not-ashish@users.noreply.github.com>
2024-01-02 12:23:34 +0530

    font change in home js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <div className='m-0 p-0' style={myStyle} >
       <h1 style={{ paddingTop: "20vh", fontSize: "92px", fontFamily: "Rubik Maps" }} className='animated-text3 mx-3'>Ashish Kumar</h1>
-      <h2 style={{ fontSize: "80px", fontFamily: "Coolvetica", textShadow: "2px 4px black" }} className='animated-text4 blk2'>Portfolio</h2>
+      <h2 style={{ fontSize: "80px", fontFamily: "'Diphylleia', serif", textShadow: "2px 4px black" }} className='animated-text4 blk2'>Portfolio</h2>
     </div>
   )
 }
